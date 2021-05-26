1140f359cda9b5b5d55eef8256950c60d9b41808
Chae Lin Shin <chaelinshin96@gmail.com>
2021-05-26 13:03:30 +0900

    Add PostMapping

---
+++ post/src/main/java/com/example/post/controller/PostAPIController.java
@@ -0,0 +1,19 @@
+package com.example.post.controller;
+
+import com.example.post.dto.PostRequestDto;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+public class PostAPIController {
+
+    @PostMapping("/post")
+    public void post(@RequestBody PostRequestDto requestData) {
+        System.out.println(requestData);
+    }
+}
